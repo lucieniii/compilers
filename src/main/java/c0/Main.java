d43191d3899e2f45d92490403a858d37dadf8d51
lucieniii <lucienli0320@gmail.com>
2020-12-17 04:07:09 +0800

    init c0

---
+++ src/main/java/c0/Main.java
@@ -0,0 +1,9 @@
+package main.java.c0;
+import main.java.c0.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        new TestUtil().runUtil();
+    }
+}
---
+++ src/main/java/c0/util/TestUtil.java
@@ -0,0 +1,10 @@
+package main.java.c0.util;
+
+public class TestUtil {
+
+    String msg = "This is TestUtil.";
+
+    public void runUtil () {
+        System.out.println(this.msg);
+    }
+}
