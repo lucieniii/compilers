d32dc4b9f3eb4af8ed1837727a18e6b557f6e767
lucieniii <lucienli0320@gmail.com>
2020-12-26 02:16:07 +0800

    Compiler v2.4 fix function bin

--- src/main/java/c0/Main.java
+++ src/main/java/c0/Main.java
@@ -95,8 +95,8 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        traverseData("data");
+        //traverseData("data");
         //traverseData("onetest");
-        //testData(args[0], args[1]);
+        testData(args[0], args[1]);
     }
 }
